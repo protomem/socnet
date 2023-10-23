65c727ebeaa790fef0a6ea89cf43c83cc0f3bf9a
protomem <protomem0709@gmail.com>
2023-10-23 15:58:30 +0300

    feat: define queries for users

--- profile-service/internal/domain/port/queries.go
+++ profile-service/internal/domain/port/queries.go
@@ -1,5 +1,54 @@
 package port
 
-import "context"
+import (
+	"context"
 
-type QueryFunc[Param, Result any] func(ctx context.Context, param Param) (Result, error)
+	"github.com/google/uuid"
+	"github.com/protomem/socnet/profile-service/internal/domain/model"
+)
+
+type (
+	QueryFunc[Params any] func(ctx context.Context, params Params) error
+
+	QueryWithResultFunc[Param, Result any] func(ctx context.Context, param Param) (Result, error)
+)
+
+type (
+	GetUserQuery QueryWithResultFunc[uuid.UUID, model.User]
+)
+
+type (
+	GetUserByNicknameQuery QueryWithResultFunc[string, model.User]
+)
+
+type (
+	GetUserByEmailQuery QueryWithResultFunc[string, model.User]
+)
+
+type (
+	FindUsersQuery QueryWithResultFunc[[]uuid.UUID, []model.User]
+)
+
+type (
+	CreateUserParams struct {
+		Nickname string
+		Email    string
+		Password string
+	}
+
+	CreateUserQuery QueryWithResultFunc[CreateUserParams, model.User]
+)
+
+type (
+	UpdateUserByNicknameParams struct {
+		Nickname string
+		Email    string
+		Password string
+	}
+
+	UpdateUserByNicknameQuery QueryWithResultFunc[UpdateUserByNicknameParams, model.User]
+)
+
+type (
+	DeleteUserByNicknameQuery QueryFunc[string]
+)
